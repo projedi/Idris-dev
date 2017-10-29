8ad42f4010a63e24e155c0a5e02e61914731f1be
Alexander Shabalin <shabalyn.a@gmail.com>
2017-10-29 17:45:29 +0300

    Copy-paste elabRewrite implementation

--- src/Idris/Elab/Rewrite.hs
+++ src/Idris/Elab/Rewrite.hs
@@ -9,6 +9,8 @@ Maintainer  : The Idris Community.
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 module Idris.Elab.Rewrite(elabRewrite, elabRewriteLemma, elabRewriteInPattern) where
 
+import Debug.Trace
+
 import Idris.AbsSyntax
 import Idris.Core.Elaborate
 import Idris.Core.Evaluate
@@ -259,4 +261,79 @@ mkLemma info lemma tcon ps ty =
     bindIdxs _ _ _ _ tm = tm
 
 elabRewriteInPattern :: (PTerm -> ElabD ()) -> IState -> FC -> Maybe Name -> PTerm -> PTerm -> Maybe PTerm -> ElabD ()
-elabRewriteInPattern elab ist fc substfn_in rule sc_in newg = undefined
+elabRewriteInPattern elab ist fc substfn_in rule sc_in newg
+        = do attack
+             sc <- case newg of
+                      Nothing -> return sc_in
+                      Just t -> do
+                         letn <- getNameFrom (sMN 0 "rewrite_result")
+                         return $ PLet fc RigW letn fc t sc_in
+                                       (PRef fc [] letn)
+             tyn <- getNameFrom (sMN 0 "rty")
+             claim tyn RType
+             valn <- getNameFrom (sMN 0 "rval")
+             claim valn (Var tyn)
+             letn <- getNameFrom (sMN 0 "_rewrite_rule")
+             letbind letn RigW (Var tyn) (Var valn)
+             focus valn
+             elab rule
+             compute
+             g <- goal
+             (tmv, rule_in) <- get_type_val (Var letn)
+             env <- get_env
+             let ttrule = normalise (tt_ctxt ist) env rule_in
+             rname <- unique_hole (sMN 0 "replaced")
+             case unApply ttrule of
+                  (P _ (UN q) _, [lt, rt, l, r]) | q == txt "=" ->
+                     do substfn <- findSubstFn substfn_in ist lt rt
+                        let pred_tt = mkP (P Bound rname rt) l r g
+                        when (g == pred_tt) $ lift $ tfail (NoRewriting l r g)
+                        let pred = PLam fc rname fc Placeholder
+                                        (delab ist pred_tt)
+                        let rewrite = addImplBound ist (map fstEnv env) (PApp fc (PRef fc [] substfn)
+                                           [pexp (stripImpls pred),
+                                            pexp (stripImpls rule), pexp sc])
+                        trace ("LHS: " ++ show l ++ "\n" ++
+                               "RHS: " ++ show r ++ "\n" ++
+                               "REWRITE: " ++ show rewrite ++ "\n" ++
+                               "GOAL: " ++ show (delab ist g)) $
+                         elab rewrite
+                        solve
+                  _ -> lift $ tfail (NotEquality tmv ttrule)
+      where
+        mkP :: TT Name -> -- Left term, top level
+               TT Name -> TT Name -> TT Name -> TT Name
+        mkP lt l r ty | l == ty = lt
+        mkP lt l r (App s f a)
+            = let f' = if (r /= f) then mkP lt l r f else f
+                  a' = if (r /= a) then mkP lt l r a else a in
+                  App s f' a'
+        mkP lt l r (Bind n b sc)
+             = let b' = mkPB b
+                   sc' = if (r /= sc) then mkP lt l r sc else sc in
+                   Bind n b' sc'
+            where mkPB (Let rig t v)
+                       = let t' = if (r /= t) then mkP lt l r t else t
+                             v' = if (r /= v) then mkP lt l r v else v in
+                             Let rig t' v'
+                  mkPB b = let ty = binderTy b
+                               ty' = if (r /= ty) then mkP lt l r ty else ty in
+                                     b { binderTy = ty' }
+        mkP lt l r x = x
+
+        -- If we're rewriting a dependent type, the rewrite type the rewrite
+        -- may break the indices.
+        -- So, for any function argument which can be determined by looking
+        -- at the indices (i.e. any constructor guarded elsewhere in the type)
+        -- replace it with a _. e.g. (++) a n m xs ys becomes (++) _ _ _ xs ys
+        -- because a,n, and m are constructor guarded later in the type of ++
+
+        -- FIXME: Currently this is an approximation which just strips
+        -- implicits. This is perfectly fine for most cases, but not as
+        -- general as it should be.
+        stripImpls tm = mapPT phApps tm
+
+        phApps (PApp fc f args) = PApp fc f (map removeImp args)
+          where removeImp tm@(PImp{}) = tm { getTm = Placeholder }
+                removeImp t = t
+        phApps tm = tm
